2732f28491210ac3b4d89a88026eb789bd6d7aef
Vinit <vinit_motghare@outlook.com>
2025-05-29 16:56:39 +0530

    Tried Scraping a Reddit post for Traits of Good Dev

---
+++ data/plotting_trait.py
@@ -0,0 +1,34 @@
+import re
+from collections import Counter
+
+# Loading the trait file
+with open("data/Trait_forGoodDev.txt", "r", encoding="utf-8") as f:
+    text = f.read().lower()
+
+# list of key traits
+traits = [
+    "problem solving", "passionate", "communication", "debugging", "ownership",
+    "resilience", "teamwork", "consistency", "fast thinker", "documentation",
+    "business understanding", "asking questions", "design decisions",
+    "not giving up", "adaptability", "deep thinking"
+]
+
+# Count occurrences
+counts = Counter()
+for trait in traits:
+    counts[trait] = len(re.findall(trait, text))
+
+# Filterin traits with at least 1 count
+filtered = {k: v for k, v in counts.items() if v > 0}
+
+# Plotting
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12, 6))
+plt.bar(filtered.keys(), filtered.values(), color="skyblue")
+plt.xticks(rotation=45, ha="right")
+plt.title("Most Mentioned Traits of Great Developers")
+plt.xlabel("Trait")
+plt.ylabel("Mentions")
+plt.tight_layout()
+plt.show()
---
+++ scripts/scraping_goodDev_traits.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests,os
+from lxml import etree
+
+uri = 'https://www.reddit.com/r/developersIndia/comments/1igteg0/common_traits_of_a_great_developer_you_have/'
+request = requests.get(uri)
+html_content = request.text
+soup = BeautifulSoup(html_content, 'lxml')
+post_title = soup.find('h1').text.strip()
+with open('data/Trait_forGoodDev.txt', 'w', encoding='utf-8') as f:
+    pass  # Just opening in 'w' mode clears the file
+
+with open('data/Trait_forGoodDev.txt', 'a') as f:
+    f.write(f"Post title : {post_title} \n\n")
+
+
+import praw 
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("SECRET_KEY"),
+    username=os.getenv("API_USERNAME"),
+    password=os.getenv("API_PASSWORD"),
+    user_agent='MyAPI/0.0.1'
+)
+
+submission = reddit.submission(url=uri)
+submission.comments.replace_more(limit=0)
+
+
+with open('data/Trait_forGoodDev.txt', 'a', encoding='utf-8') as f:
+    for comment in submission.comments[:50]:
+        comment_text = comment.body
+        
+        # Remove blank lines 
+        cleaned_text = "\n".join(
+            [line.strip() for line in comment_text.splitlines() if line.strip()]
+        )
+        if re.search(r'https?://\S+', comment_text): ## Skipping comments with contains links
+            continue
+        
+        f.write(f'Comment: {cleaned_text}\n\n')
+
+print('Data saved to txt file')
